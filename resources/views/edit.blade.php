0244c5391c18e2d19f0d59f93dbb55ff6bc62020
Damian Budzik <budzik.damian@gmail.com>
2023-01-14 10:11:01 +0100

    14.01

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -14,13 +14,15 @@
             </div>
         @endif
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-{{--                <div class="pb-2">--}}
-{{--                    <a href="{{ backToPreviousURL }}" class="text-blue-700 ">--}}
-{{--                        <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="submit">--}}
-{{--                            WRÓĆ--}}
-{{--                        </button></a>--}}
+                <div class="pb-2">
+                    <form id="goback" method="GET" action="{{ url()->previous() }}">
+                    <a class="text-blue-700 ">
+                        <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="submit">
+                            WRÓĆ
+                        </button></a>
+                    </form>
 
-{{--                </div>--}}
+                </div>
             </div>
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg dark:bg-gray-800">
