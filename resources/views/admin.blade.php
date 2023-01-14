7dd0d2208041e3b857ab05ce2d1c461b7621ee89
Damian Budzik <budzik.damian@gmail.com>
2023-01-14 09:58:43 +0100

    14.01

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -35,6 +35,12 @@
                                     aria-controls="profile" aria-selected="true">Dodaj Produkt</button>
                             </li>
 
+                            <li class="mr-2" role="presentation">
+                                <button
+                                    class="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg hover:text-blue-600 dark:text-blue-500 dark:hover:text-blue-500 dark:border-blue-500"
+                                    id="dodajPartie-button" data-tabs-target="#dodajPartie" type="button" role="tab"
+                                    aria-controls="profile" aria-selected="true">Dodaj Partię</button>
+                            </li>
 
                         </ul>
                     </div>
@@ -130,6 +136,20 @@
                                 <input type="file" name="image" style="display: none;" id="photoAddInput">
                             </form>
                         </div>
+                        <div class="p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="dodajPartie" role="tabpanel"
+                             aria-labelledby="dodajPartie-button">
+                            <div class="relative block w-80">
+                                <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none max-w-xs">
+                                    <svg class="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                                    <span class="sr-only">Search icon</span>
+                                </div>
+                                <input type="text" data-dropdown-toggle="" id="" class="block p-2.5 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" autocomplete="off" placeholder="Wyszukaj Partię...">
+{{--                                                                    <button type="submit" id="search-category-button" name="category" class="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add</button>--}}
+                            </div>
+                            <div id="" class="hidden bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 max-w-xs mt-2 w-80">
+                                <ul id="" class="py-1 text-sm text-gray-700 dark:text-gray-200"></ul>
+                            </div>
+                        </div>
 
                     </div>
 
@@ -142,8 +162,7 @@
     </div>
     <script type="text/javascript">
         let user_id = {{Auth::id()}};
-        let add_admin_url = "{{route('add_admin', 0)}}".slice(0, -1)
-        let remove_admin_url = "{{route('remove_admin', 0)}}".slice(0, -1)
+
 
 
         let disableOnUnload = false
